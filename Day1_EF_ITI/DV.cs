b23109c57df9b88ef6797a150da768955fd6a2c7
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-28 21:10:16 +0200

    Initial commit

---
+++ Day1_EF_ITI/DV.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Day1_EF_ITI.Context;
+using Day1_EF_ITI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day1_EF_ITI
+{
+    public partial class DV : Form
+    {
+        public DV()
+        {
+            InitializeComponent();
+            this.FormClosed += (sender, e) => Context.Dispose();
+
+        }
+
+
+        BindingNavigator bindingNavigator;
+        BindingSource BSrc;
+        BindingSource PS;
+        PubsContext Context = new PubsContext();
+        
+        private void DV_Load(object sender, EventArgs e)
+        {
+            Context.Titles.Load();
+            Context.Publishers.Load();
+            BSrc = new BindingSource(Context.Titles.Local.ToBindingList(), ""); //if not try just the list
+            PS = new BindingSource(Context.Publishers.Local.ToBindingList(), ""); //if not try just the list
+
+            //Controls 
+            BindingSource b = new BindingSource();
+
+            title_id.DataBindings.Add("Text", BSrc, "TitleId", true);
+            title.DataBindings.Add("Text", BSrc, "Title1", true);
+            type.DataBindings.Add("Text", BSrc, "Type", true);
+            price.DataBindings.Add("Value", BSrc, "Price", true);
+            advance.DataBindings.Add("Value", BSrc, "Advance", true);
+            royalty.DataBindings.Add("Value", BSrc, "Royalty", true);
+            sales.DataBindings.Add("Value", BSrc, "YtdSales", true);
+            notes.DataBindings.Add("Text", BSrc, "Notes", true);
+            pubDate.DataBindings.Add("Value", BSrc, "Pubdate", true);
+
+
+            b.AddingNew += (sender, e) => b.AddNew();
+            ///Navigator
+            bindingNavigator = new BindingNavigator(true);
+            this.Controls.Add(bindingNavigator);
+            bindingNavigator.Dock = DockStyle.Bottom;
+            bindingNavigator.BindingSource = BSrc;
+
+            comboBox1.DataSource = PS; //new Data Table
+            comboBox1.DisplayMember = "PubName"; //pub name from the Binding Source from new Data Table
+            comboBox1.ValueMember = "PubId";
+            comboBox1.DataBindings.Add("SelectedValue", BSrc, "PubId", true);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            BSrc.EndEdit();
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ Day1_EF_ITI/GV.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Day1_EF_ITI.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Day1_EF_ITI
+{
+    public partial class GV : Form
+    {
+        public GV()
+        {
+            InitializeComponent();
+            this.FormClosed += (sender, e) => Context.Dispose();
+
+            //this.FormClosing+= (sender, e) => Context.Dispose();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        PubsContext Context = new PubsContext();
+       
+        private void GV_Load(object sender, EventArgs e)
+        {
+
+           
+                //load all data in datagridview
+                Context.Titles.Load();
+                dataGridView1.DataSource = Context.Titles.Local.ToBindingList();
+
+
+                Context.Publishers.Load();
+                DataGridViewComboBoxColumn ck = new DataGridViewComboBoxColumn();
+                ck.DataSource = Context.Publishers.Local.ToBindingList();
+                ck.HeaderText = "Publisher";
+                ck.ValueMember = "PubId";
+                ck.DisplayMember = "PubName";
+                ck.DataPropertyName = "PubId";
+                dataGridView1.Columns.Add(ck);
+
+                dataGridView1.Columns["PubId"].Visible = false;
+                dataGridView1.Columns["Pub"].Visible = false;
+
+        }
+
+        private void btnDGVSave_Click(object sender, EventArgs e)
+        {
+            dataGridView1.EndEdit();
+            Context.SaveChanges();
+        }
+    }
+}
+//dataGridView1.UserDeletingRow += (args, e) =>
+//{
+//    var idToDelete = (String)e.Row.Cells["TitleId"].Value;
+//    //try
+//    //{
+
+//    //    Context.Titles.Remove((Entities.Title)(from T in Context.Titles
+//    //                                           where T.TitleId == idToDelete
+//    //                                           select T).FirstOrDefault());
+//    //    Context.SaveChanges();
+//    //}
+//    //catch (Exception ex)
+//    //{
+//    //    Trace.WriteLine(ex.Message);
+//    //}
+
+
+//};
